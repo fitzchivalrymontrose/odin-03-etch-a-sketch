05054a01362b59a4ad14716f83a2c64505ddb422
fitzchivalrymontrose <fitzchivalrymontrose@protonmail.com>
2021-12-23 17:43:27 -0500

    made button to change fill method to random rgb color on every mouseover

--- etch.js
+++ etch.js
@@ -1,3 +1,17 @@
+// const gridSquare = document.createElement('div');
+// gridSquare.classList.add('square');
+// gridSquare.style.width = '80px';
+// gridSquare.style.height = '80px';
+// gridSquare.style.backgroundColor = 'orange';
+// gridSquare.textContent = 'is it working?'
+// let square = makeSquare(16);
+
+// Change color of square
+// function fillSquare(e) {
+//     e.target.id = 'square-changed';
+// }
+//////////////////////////////////////////////////////
+
 const sketchBox = document.querySelector('.sketch-box-inner');
 
 sketchBox.appendChild(makeGrid(16));
@@ -14,38 +28,51 @@ function makeNewGrid () {
     const size = prompt('Size of Grid?');
     const item = sketchBox.querySelector('.grid');
     sketchBox.replaceChild(makeGrid(size), item);
-    
     for (let i = 0; i < squares.length; i++) {
         squares[i].removeEventListener('mouseover', switchClasses);
     }
-
     squares = document.querySelectorAll('.square');
     for (let i = 0; i < squares.length; i++) {
         squares[i].addEventListener('mouseover', switchClasses);
     }
 }
 
-// Change color of square
-
-
-function fillSquare(e) {
-    e.target.id = 'square-changed';
-}
-
 function switchClasses (e) {
-//    e.target.classList.toggle('square');
     e.target.classList.toggle('square-changed');
     e.target.removeEventListener('mouseover', switchClasses);
 }
 
-// const gridSquare = document.createElement('div');
-// gridSquare.classList.add('square');
-// gridSquare.style.width = '80px';
-// gridSquare.style.height = '80px';
-// gridSquare.style.backgroundColor = 'orange';
-// gridSquare.textContent = 'is it working?'
 
-// let square = makeSquare(16);
+
+
+const btnChangeRandom = document.querySelector('.button-change-random');
+btnChangeRandom.addEventListener('click', changeBgRandom);
+
+
+function changeBgRandom (e) {
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].removeEventListener('mouseover', switchClasses);
+        squares[i].addEventListener('mouseover', bgRandomColor);
+    }
+}
+
+function bgRandomColor (e) {
+    const rgbColor = rgbRandom();
+    e.target.style.backgroundColor = rgbColor;
+}
+
+function rgbRandom () {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+
+
+
+
+
 
 
 function makeSquare (size) {
@@ -53,14 +80,11 @@ function makeSquare (size) {
     gridSquare.classList.add('square');    
     gridSquare.style.width = `${960 / size}px`;
     gridSquare.style.height = `${960 / size}px`;
-    
-    console.log(gridSquare);
     return gridSquare;
 }
 
 function makeColumn (size) {
     const column = document.createElement('div');
-    
     for (let i = 0; i < size; i++) {
         column.appendChild(makeSquare(size));
     }
@@ -70,11 +94,8 @@ function makeColumn (size) {
 function makeGrid(size) {
     const grid = document.createElement('div');
     grid.classList.add('grid');
-
     for (let i = 0; i < size; i++) {
         grid.appendChild(makeColumn(size));
     }
-
-    
     return grid;
 }
