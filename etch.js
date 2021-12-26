0dc2ec9506fe4eb34173f6076026da209afea599
fitzchivalrymontrose <fitzchivalrymontrose@protonmail.com>
2021-12-26 11:14:43 -0500

    Clean up, removed unused function and its references in JS, and classes in CSS.

--- etch.js
+++ etch.js
@@ -21,9 +21,8 @@ let currentBrush = '';
 
 let squares = document.querySelectorAll('.square');
 for (let i = 0; i < squares.length; i++) {
-    squares[i].addEventListener('mouseover', switchClasses);
+    squares[i].addEventListener('mouseover', changerClassic);
 }
-
 
 function changeBrushTo (brush) {
     clearListeners(); 
@@ -39,18 +38,17 @@ function changeBrushTo (brush) {
         default: break;
     }
 }
-
 function clearListeners () {
     for (let i = 0; i < squares.length; i++) {
         squares[i].removeEventListener('mouseover', eraseSquare);
-        squares[i].removeEventListener('mouseover', switchClasses);
+        //squares[i].removeEventListener('mouseover', switchClasses);
         squares[i].removeEventListener('mouseover', bgRandomColor);
         squares[i].removeEventListener('mouseover', fadeIn);
         squares[i].removeEventListener('mouseover', changerClassic);
     }
 }
-
-// new grid button 
+//
+/// new grid button 
 const clearBtn = document.querySelector('button');
 clearBtn.addEventListener('click', makeNewGrid);
 function makeNewGrid () {
@@ -58,7 +56,7 @@ function makeNewGrid () {
     const item = sketchBox.querySelector('.grid');
     sketchBox.replaceChild(makeGrid(size), item);
     for (let i = 0; i < squares.length; i++) {
-        squares[i].removeEventListener('mouseover', switchClasses);
+        squares[i].removeEventListener('mouseover', changerClassic);
     }
     squares = document.querySelectorAll('.square');
     for (let i = 0; i < squares.length; i++) {
@@ -78,13 +76,13 @@ function makeNewGrid () {
         }
     }
 }    
-
-
-function switchClasses (e) {
-    e.target.classList.toggle('square-changed');
-    e.target.removeEventListener('mouseover', switchClasses);
-}
-
+//
+/// deprecated
+/// function switchClasses (e) {
+//     e.target.classList.toggle('square-changed');
+//     e.target.removeEventListener('mouseover', switchClasses);
+// }
+//
 // classic black button
 const btnClassic = document.querySelector('.button-classic-black');
 btnClassic.addEventListener('click', changeClassic);
@@ -92,7 +90,7 @@ function changeClassic(e) {
     currentBrush = 'classic';
     for (let i = 0; i < squares.length; i++) {
         squares[i].removeEventListener('mouseover', eraseSquare);
-        squares[i].removeEventListener('mouseover', switchClasses);
+        //squares[i].removeEventListener('mouseover', switchClasses);
         squares[i].removeEventListener('mouseover', bgRandomColor);
         squares[i].removeEventListener('mouseover', fadeIn);
         squares[i].addEventListener('mouseover', changerClassic);
@@ -101,29 +99,23 @@ function changeClassic(e) {
 function changerClassic(e) {
     e.target.style.backgroundColor = 'black';
     //sketchBoxBorder.style.backgroundColor = 'black';
-    e.target.classList.value = 'square-changed';
+    //e.target.classList.value = 'square-changed';
     e.target.removeEventListener('click', changerClassic);
 }
-
-
-
-
-
-// random color button
+//
+/// random color button
 const btnChangeRandom = document.querySelector('.button-change-random');
 btnChangeRandom.addEventListener('click', changeBgRandom);
-
 function changeBgRandom (e) {
     currentBrush = 'rainbow';
     for (let i = 0; i < squares.length; i++) {
         squares[i].removeEventListener('mouseover', eraseSquare);
-        squares[i].removeEventListener('mouseover', switchClasses);
+        //squares[i].removeEventListener('mouseover', switchClasses);
         squares[i].removeEventListener('mouseover', fadeIn);
         squares[i].removeEventListener('mouseover', changerClassic);
         squares[i].addEventListener('mouseover', bgRandomColor);
     }
 }
-
 function bgRandomColor (e) {
     const rgbColor = rgbRandom();
     e.target.style.backgroundColor = rgbColor;
@@ -137,15 +129,15 @@ function rgbRandom () {
     const b = Math.floor(Math.random() * 256);
     return `rgb(${r}, ${g}, ${b})`;
 }
-
-// darken by 10 percent each pass button
+//
+/// darken each pass button
 const fadeBtn = document.querySelector('.button-change-fade');
 fadeBtn.addEventListener('click', changeFadeBrush);
 function changeFadeBrush (e) {
     currentBrush = 'fade';
     for (let i = 0; i < squares.length; i++) {
         squares[i].removeEventListener('mouseover', eraseSquare);
-        squares[i].removeEventListener('mouseover', switchClasses);
+        //squares[i].removeEventListener('mouseover', switchClasses);
         squares[i].removeEventListener('mouseover', bgRandomColor);
         squares[i].removeEventListener('mouseover', changerClassic);
         squares[i].addEventListener('mouseover', fadeIn );
@@ -156,14 +148,14 @@ function fadeIn (e) {
     //sketchBoxBorder.style.backgroundColor = 'darkgray';
     e.target.removeEventListener('click', fadeIn);
 }
-
-// eraser button
+//
+/// eraser button
 const eraserBtn = document.querySelector('.button-eraser');
 eraserBtn.addEventListener('click', turnOnEraser);
 function turnOnEraser(e) {
     currentBrush = 'eraser';
     for (let i = 0; i < squares.length; i++) {
-        squares[i].removeEventListener('mouseover', switchClasses);
+        //squares[i].removeEventListener('mouseover', switchClasses);
         squares[i].removeEventListener('mouseover', bgRandomColor);
         squares[i].removeEventListener('mouseover', changerClassic);
         squares[i].removeEventListener('mouseover', fadeIn );
@@ -174,9 +166,8 @@ function eraseSquare(e) {
     e.target.style.backgroundColor = 'white';
     e.target.removeEventListener('mouseover', eraseSquare);
 }
-
-
-// generate grid
+//
+/// generate inital grid
 function makeSquare (size) {
     const gridSquare = document.createElement('div');
     gridSquare.classList.add('square');    
